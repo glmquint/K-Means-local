8c55ac65e7ceb48f24943a613d33bbe121577a5f
Guillaume Quint <g.quint@studenti.unipi.it>
2023-04-07 17:53:11 +0200

    add python script for dataset generation

---
+++ dataset_generator.py
@@ -0,0 +1,23 @@
+from random import randint, gauss, randrange
+from sys import argv
+#import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    NUM_CENTERS = int(argv[1])
+    NUM_POINTS = int(argv[2])
+    x = []
+    y = []
+    for c in range(NUM_CENTERS):
+        cx = randint(-100, 100)
+        cy = randint(-100, 100)
+        s = randrange(1, 10)
+        for p in range(NUM_POINTS):
+            coord = []
+            #for d in range(2):
+                #coord.append(gauss(cx,s))
+            x.append(gauss(cx, s))
+            y.append(gauss(cy, s))
+    #plt.scatter(x, y)
+    for px, py in zip(x, y):
+        print(f"{px},{py}")
+
