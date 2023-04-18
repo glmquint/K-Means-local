1b1f851fb39ec4aa0de4ec186dba922e4c05726c
Guillaume Quint <g.quint@studenti.unipi.it>
2023-04-18 19:53:44 +0200

    change dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -3,8 +3,12 @@ from sys import argv
 #import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    if len(argv) < 4:
+        print("usage!")
+        quit()
     NUM_CENTERS = int(argv[1])
     NUM_POINTS = int(argv[2])
+    out_file = argv[3]
     x = []
     y = []
     for c in range(NUM_CENTERS):
@@ -18,6 +22,7 @@ if __name__ == "__main__":
             x.append(gauss(cx, s))
             y.append(gauss(cy, s))
     #plt.scatter(x, y)
-    for px, py in zip(x, y):
-        print(f"{px},{py}")
+    with open(out_file, 'w') as f:
+        for px, py in zip(x, y):
+            f.write(f"{px},{py}\n")
 
