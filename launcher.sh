fe235ec002e9c2fe5174275a783ba7a6256be9ca
Guillaume Quint <g.quint@studenti.unipi.it>
2023-05-14 18:14:51 +0200

    update launcher

--- launcher.sh
+++ launcher.sh
@@ -11,8 +11,8 @@ dsets=(
 for ((i=0; i < ${#dsets[@]}; ++i));
 do
 	dataset="${dsets[$i]}"
-	#for threads in $(seq 1 20);
-	for threads in $(seq -f "%05g" 1000 1000 20000);
+	for threads in $(seq -f "%02g" 1 20);
+	#for threads in $(seq -f "%05g" 1000 1000 20000);
 	do
 		echo ${dataset} ${threads}
 		K-means_project/x64/Release/K-means_project.exe dataset/${dataset}.serialized 5 ${threads} > results-old/${i}-${dataset}-${threads}.csv
